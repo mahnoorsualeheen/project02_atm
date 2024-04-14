145bbb6a554e5f93c3c40a5d5152a807cc92fff2
Mahnoor Malik <160276957+mahnoorsualeheen@users.noreply.github.com>
2024-04-14 09:16:54 +0500

    Add files via upload
    Project02_atm

---
+++ atm.ts
@@ -0,0 +1,133 @@
+#!/usr/bin/env node
+
+
+console.log("_______________________________");
+console.log("**********ATM MACHINE**********");
+console.log("_______________________________");
+
+//Starting balance
+let myBalance: number = 15000;
+
+// Pin code 
+let myPin: number = 1234;
+
+
+import inquirer from "inquirer"
+let pinNo = await inquirer.prompt(
+    {
+        message : "Enter your Pin Code : ",
+        name: "pin",
+        type: "number"
+    }
+)
+//Checking Pin 
+
+if(pinNo.pin === myPin)
+{
+    let operationAns = await inquirer.prompt(
+     {
+        message: "Select option you wants to do",
+        type: "list",
+        choices: ["withdraw", "checkBalance", "fastCash", "changePin"],
+        name: "operation",
+     }   
+    )
+
+    if(operationAns.operation == "withdraw")
+    {
+        let amountAns = await inquirer.prompt(
+            {
+                message : "Enter amount : ",
+                type : "number",
+                name: "amount",
+            }
+        )
+        {
+
+        if (amountAns.amount <= 20000)
+        {
+            myBalance -= amountAns.amount;
+            console.log(`Your remaining balance is ${myBalance}`)
+        }
+        else{
+            console.log("Insufficient Balance")
+        }
+    }
+}
+    else if(operationAns.operation === "checkBalance")
+    {
+console.log(`Your Balnace is ${myBalance}`)
+       }
+    else if(operationAns.operation === "fastCash")
+    {
+        let fast = await inquirer.prompt(
+            {
+                message: "Fast Cast withdrawal ",
+                name: "fastCash",
+                type: "list",
+                choices: ["1000", "2000", "5000", "10000"]
+            }
+        )
+        if (myBalance > fast.fastCash && fast.fastCash === "1000")
+            {
+                myBalance -= fast.fastCash
+                console.log(`Your remaining balace is ${myBalance}`)
+            }
+            else if(myBalance > fast.fastCash && fast.fastCash === "2000")
+                {
+                myBalance -= fast.fastCash
+            console.log(`Your remaining balace is ${myBalance}`)
+                }
+                else if (myBalance > fast.fastCash && fast.fastCash === "5000")
+                    {
+                        myBalance -= fast.fastCash
+                    console.log(`Your remaining balace is ${myBalance}`)
+                    }
+                    else if (myBalance > fast.fastCash && fast.fastCash === "10000")
+                        {
+                            myBalance -= fast.fastCash
+                        console.log(`Your remaining balace is ${myBalance}`)
+                        }
+                        else
+                    {
+                    console.log("Insufficient Balance")
+                    }
+    }
+    else {
+        let oldPin = await inquirer.prompt(
+            {
+                message: "Enter your Pin : ",
+                type: "number",
+                name: "pin"
+            }
+            
+        )
+        if (oldPin.pin === myPin)
+            {
+                let newpin = await inquirer.prompt(
+                    {
+                        message: "Enter your New Pin: ",
+                        type: "number",
+                        name : "newPin"
+                    }
+                )
+                myPin = newpin.newPin
+                console.log(`Your New Pin Code is ${myPin}`)
+            }
+            else {
+                console.log("Invalid Pin code")
+            }
+    }
+    }
+
+
+else 
+
+{
+    console.log("Invalid Pin code")
+}
+
+
+
+
+
